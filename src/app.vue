b6af6a83884caa9a91ffa93a923068f93fd2d9e0
sinpro <1040680627@qq.com>
2020-05-15 11:56:02 +0800

    feature:一个webpack 4.0以上的vue项目

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1>wpvue1111</h1>
+	</div>
+</template>
+<script>
+export default {
+	data(){
+		return {
+			
+		}
+	},
+	mounted(){
+		if(process.env.NODE_ENV==='development'){
+			window.app=this;
+		}
+	},
+}	
+</script>
+<style lang="scss" scoped>
+	
+</style>
