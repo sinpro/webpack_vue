f53869d3181ec59b351267fc183a2384ae9207a8
sinpro <1040680627@qq.com>
2020-07-14 17:57:07 +0800

    fix:webpack.con.js优化

--- config/index.js
+++ config/index.js
@@ -9,8 +9,8 @@ module.exports = {
   mockPath            : resolve(__dirname, '../mock'), // 项目源代码目录
   srcPath             : resolve(__dirname, '../src'), // 项目源代码目录
   rootPath            : resolve(__dirname, '../dist'), // production环境打包后静态资源根目录
-  fatDistPath         : resolve(__dirname, '../dist/fat-prod'),// 测试环境打包后的路径
-  prodDistPath         : resolve(__dirname, '../dist/prod-prod'),// 生产环境打包后的路径
+  fatDistPath         : resolve(__dirname, '../dist/fat-dist'),// 测试环境打包后的路径
+  uatDistPath         : resolve(__dirname, '../dist/uat-dist'),// 生产环境打包后的路径
   limit               : 1024 * 5, // url-loader limit参数
   devServer           : devServer,
   bundleAnalyzer      : false,
